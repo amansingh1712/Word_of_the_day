9ca5f4819c59a710f81d8a1a42e5ea817c8a0110
Aman Singh <aman.iit96@gmail.com>
2022-10-17 22:54:26 +0530

    Basic logic implemented

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 
+const fs = require("fs");
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log("err:", err);
+    } else {
+      const words = JSON.parse(data);
+      res.send([words[Math.floor(Math.random() * words.length)]]);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on Port : ${PORT}`);
